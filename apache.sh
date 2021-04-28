dba7b938d0931a347292df392abab1dac0c154a7
JB <onfroy@gmx.com>
2021-04-28 16:18:04 +0200

    Script is viable

--- apache.sh
+++ apache.sh
@@ -4,6 +4,8 @@
 # Version: 1.0
 #
 
+sudo pacman -Syu apache php-apache php-fpm php-cgi mod_fcgid
+
 while getopts d:a:f: flag
 do
     case "${flag}" in
@@ -11,17 +13,26 @@ do
         a) age=${OPTARG};;
         f) fullname=${OPTARG};;
     esac
-done$
+done
 
 ## Create VHosts folders
 sudo mkdir -pv /srv/http/ap1
 sudo mkdir -pv /srv/http/ap2
 
 ## Extract mods and configuration files to the conf folder
-sudo cp /etc/http/conf/httpd.conf /etc/http/conf/httpd.default.conf
+sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.default.conf
 sudo tar -xvf conf/apache-2.4.46.tar.gz -C /etc/httpd/conf
 
-#TO DO
-sed -i 's/devserver/"$domainname"/g' /etc/hosts
-sed -i 's/devserver/dev/g' /etc/httpd/conf/vhosts/ap1.conf
-sed -i 's/devserver/dev/g' /etc/httpd/conf/vhosts/ap2.conf
+## Copy test pages to doc roots
+sudo cp index.* /srv/http/ap1/
+sudo cp index.* /srv/http/ap2/
+
+## Change html page to display vhost name
+sudo sed -i 's/Apache!/Apache!AP1/g' /srv/http/ap1/index.html
+sudo sed -i 's/Apache!/Apache!AP2/g' /srv/http/ap2/index.html
+
+##  Replace domain name in all config files to separate dev/test/prod
+sed -i "s/devserver/"$domainname"/g" /etc/hosts
+sed -i "s/devserver/"$domainname"/g" /etc/httpd/conf/httpd.conf
+sed -i "s/devserver/"$domainname"/g" /etc/httpd/conf/vhosts/ap1.conf
+sed -i "s/devserver/"$domainname"/g" /etc/httpd/conf/vhosts/ap2.conf
