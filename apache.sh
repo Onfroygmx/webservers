31003f69eea1b7db6fb3566a4079e0769bb20add
JB <onfroy@gmx.com>
2021-04-28 13:47:28 +0200

    Added folder creation structure

--- apache.sh
+++ apache.sh
@@ -3,3 +3,10 @@
 # Author : Onf
 # Version: 1.0
 #
+## Create VHosts folders
+sudo mkdir -pv /srv/http/ap1
+sudo mkdir -pv /srv/http/ap2
+
+## Extract mods and configuration files to the conf folder
+sudo cp /etc/http/conf/httpd.conf /etc/http/conf/httpd.default.conf
+tar -xvf apache.tar.gz -C /etc/httpd/conf
